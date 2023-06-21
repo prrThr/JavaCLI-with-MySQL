715fddacdfe2a234a8e04f083d3b8013bbed0403
prrThr <arthur.op9@gmail.com>
2023-06-21 16:21:00 -0300

    conexao aparentemente funcionando

--- src/main/java/org/example/MYSQLConnection.java
+++ src/main/java/org/example/MYSQLConnection.java
@@ -1,2 +1,16 @@
-package org.example;public class MYSQLConnection {
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MYSQLConnection {
+    public Connection conectaBD() throws SQLException, ClassNotFoundException {
+        String url = "jdbc:mysql://localhost:3306/M3?useSSL=false&serverTimezone=UTC";
+        String user = "root";
+        String passwd = "galinha20";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(url, user, passwd);
+    }
+
 }
