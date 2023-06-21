b50bd1ebbde4d16461129c3ff89b817e729248ca
prrThr <arthur.op9@gmail.com>
2023-06-21 17:34:10 -0300

    readme adicionado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,11 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        // Conectando com o banco de dados
         Connection connection = new MYSQLConnection().conectaBD();
         Statement statement = connection.createStatement();
 
+        // Fazendo chamada de consulta (SELECT * FROM ...)
         SelectAll selectAll = new SelectAll();
         selectAll.Consulta(connection, statement, "Unidade");
     }
